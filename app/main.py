65ccfb6c33cf50f31a39e431e07f3e488eb02d91
Amartya Sen <amartya.of.sen.is.18@gmail.com>
2026-09-03 15:23:40 +0530

    Removed trailing / at the end of the new link

--- app/main.py
+++ app/main.py
@@ -20,7 +20,7 @@ app.add_middleware(
         "http://localhost:3000",
         "https://socrates-frontend.vercel.app",
         "https://socrates-frontend-blush.vercel.app",
-        "https://socrates-frontend-amartya-sens-projects-b10cf8e1.vercel.app/"
+        "https://socrates-frontend-amartya-sens-projects-b10cf8e1.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
